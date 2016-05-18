fab54d3e4f6fa8bb6e683ed7ed90d096d6fa1da8
Lauren Budorick <lauren@mapbox.com>
2016-05-18 16:09:14 -0400

    Recreate feature/expose_node_ids

--- include/storage/shared_datatype.hpp
+++ include/storage/shared_datatype.hpp
@@ -28,6 +28,7 @@ struct SharedDataLayout
         GRAPH_NODE_LIST,
         GRAPH_EDGE_LIST,
         COORDINATE_LIST,
+        OSM_NODE_ID_LIST,
         TURN_INSTRUCTION,
         TRAVEL_MODE,
         R_SEARCH_TREE,
